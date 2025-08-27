5c607b653983364e730cea353af52f8fd8281d46
bossmoon229 <alinaignatich@gmail.com>
2025-08-27 22:43:47 +0300

    add fisrt function on button generate

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let generate = document.getElementById('generate');
-  generate.addEventListener('click', () => {
-    console.log('hello');
+document.addEventListener("DOMContentLoaded", () => {
+  let generate = document.getElementById("generate");
+  let story = document.querySelector(".story");
+  generate.addEventListener("click", () => {
+    const currentDisplay = window.getComputedStyle(story).display;
+
+    console.log(currentDisplay);
+
+    if (currentDisplay === "none") {
+      story.style.display = "block";
+    } else {
+      story.style.display = "none";
+    }
   });
 });
