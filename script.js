e3895b6ac6e8d0cf6fc23d6b3c90082d1c3c340c
bossmoon229 <alinaignatich@gmail.com>
2025-08-28 13:24:41 +0300

    button create a block with img

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   let generate = document.getElementById("generate");
   let story = document.querySelector(".story");
+  let storyBlock = document.querySelector(".story-itself");
+  let btnText = document.getElementById("generate");
+  let storyImg = document.getElementById("img-story");
   generate.addEventListener("click", () => {
     const currentDisplay = window.getComputedStyle(story).display;
 
@@ -8,8 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (currentDisplay === "none") {
       story.style.display = "block";
-    } else {
-      story.style.display = "none";
+      storyBlock.style.display = "block";
+      btnText.innerText = "Generate another story";
+      storyImg.style.display = 'block';
+      storyImg.src = '/img/fog-people.jpg';
     }
   });
 });
